7b3ea85f45e6671a9b32bf5df5b4820931c1d344
Your Name <you@example.com>
2024-05-22 18:08:07 +0600

    changed reg and login xml

--- app/src/main/java/com/example/testingmyentertainment/FirstActivity2.java
+++ app/src/main/java/com/example/testingmyentertainment/FirstActivity2.java
@@ -2,6 +2,7 @@ package com.example.testingmyentertainment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,10 +12,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class FirstActivity2 extends AppCompatActivity {
 
     Button login;
     Button reg;
+    Button logout;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +35,29 @@ public class FirstActivity2 extends AppCompatActivity {
 
         login = findViewById(R.id.loginbutton);
         reg = findViewById(R.id.regbutton);
+        logout = findViewById(R.id.logoutid);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Log.d("ffirst", "logged in");
+            String email = currentUser.getEmail();
+            Log.d("ffirst", email);
+        }
+
+
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(FirstActivity2.this, LoginActivity.class);
                 startActivity(intent);
+//                if(currentUser != null){
+//                    Log.d("ffirst", "logged in click login");
+//                    String email = currentUser.getEmail();
+//                    Log.d("ffirst", email);
+//                }
             }
         });
 
@@ -43,7 +66,22 @@ public class FirstActivity2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(FirstActivity2.this, RegistrationActivity.class);
-                startActivity(intent);
+//                startActivity(intent);
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                if(currentUser != null){
+                    Log.d("ffirst", "logged in click logout");
+                    String email = currentUser.getEmail();
+                    Log.d("ffirst", email);
+                }
+                else{
+                    Log.d("ffirst", "logged out");
+                }
             }
         });
     }
