c42e3eb66a1325b793a7d62ab144b791ec85e406
Your Name <you@example.com>
2024-05-22 21:58:34 +0600

    login reg finalized, choose activity added

---
+++ app/src/main/java/com/example/testingmyentertainment/ChooseActivity.java
@@ -0,0 +1,60 @@
+package com.example.testingmyentertainment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ChooseActivity extends AppCompatActivity {
+
+    Button music;
+    Button film;
+    TextView logout;
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_choose);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        music = findViewById(R.id.btnmusic);
+        film = findViewById(R.id.btnflm);
+        logout = findViewById(R.id.logout);
+
+        music.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChooseActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth = FirebaseAuth.getInstance();
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                mAuth.signOut();
+                Intent intent = new Intent(ChooseActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
